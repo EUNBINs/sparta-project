1fc4a6d9c3c5c2334cfb846ce50019e81082f0ce
Galmaeki <122267336+Galmaeki@users.noreply.github.com>
2023-01-10 15:05:19 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -11,24 +11,26 @@ db = client.dbsparta
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('posting.html')
+
 
 @app.route("/toyproject", methods=["POST"])
 def toyproject_post(): #제목, 내용, 닉네임, 비밀번호
-    post_title_receive = request.form['post_title_give']
+    title_receive = request.form['title_give']
     contents_receive = request.form['contents_give']
-    nickname_receive = request.form['nickname_give']
-    post_password_receive = request.form['post_password_give']
+    nickname_receive = request.form['nicanme_give']
+    password_receive = request.form['password_give']
     category_receive = request.form['category_give']
-    post_num_receive = request.form['post_num_give']
-
+    #post_num_receive = request.form['post_num_give']
+    postingnumber = list(db.toyproject.find({}, {'_id': False}))
+    post_num = len(postingnumber) + 1
     doc = {
-        'post_title' : post_title_receive,
+        'title' : title_receive,
         'contents' : contents_receive,
-        'nickname': nickname_receive,
-        'post_password': post_password_receive,
+        'nicanme': nickname_receive,
+        'password': password_receive,
         'category': category_receive,
-        'post_num': post_num_receive
+        'post_num': post_num
     }
     db.toyproject.insert_one(doc)
     return jsonify({'msg':'저장되었습니다.'})
